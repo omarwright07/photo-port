95e41a418affdeae61543c9424c32be8e92c7a9a
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-08-08 15:41:26 -0500

    Started work on the gallery

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 // import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 // function App() {
 //   return (
@@ -30,6 +31,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
